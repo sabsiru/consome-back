128dc93538ca8dfcd6442f1caf9c8388131a0021
sabsiru <zero013@naver.com>
2025-06-13 13:35:31 +0900

    ref: PointHistoryType 변경
    기존 EARN, PENALIZE는 유지하고
    히스토리저장용 설명과 관련된 enum들을 추가함
    
    #1

--- src/main/java/consome/domain/point/PointHistoryType.java
+++ src/main/java/consome/domain/point/PointHistoryType.java
@@ -1,5 +1,37 @@
 package consome.domain.point;
 
+import lombok.Getter;
+
+@Getter
 public enum PointHistoryType {
-    EARN, PENALIZE
+    /**
+     * EARN, PENALIZE 기본형임으로 사용 금지
+     */
+    EARN("포인트 적립"),
+    PENALIZE("포인트 차감"),
+
+    SIGNUP("회원가입", EARN),
+    POST_WRITE("게시글 작성", EARN),
+    COMMENT_WRITE("댓글 작성", EARN),
+    POST_LIKE("게시글 추천", EARN),
+    COMMENT_LIKE("댓글 추천", EARN),
+    POST_DISLIKE("게시글 비추천", PENALIZE),
+    COMMENT_DISLIKE("댓글 비추천", PENALIZE);
+
+    private final String description;
+    private final PointHistoryType baseType;
+
+    PointHistoryType(String description) {
+        this.description = description;
+        this.baseType = this;
+    }
+
+    PointHistoryType(String description, PointHistoryType baseType) {
+        this.description = description;
+        this.baseType = baseType;
+    }
+
+    public PointHistoryType getActualType() {
+        return this.baseType;
+    }
 }
