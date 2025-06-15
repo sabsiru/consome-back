3936eff7d15a1ee5d1b8dec6d593a2c9355933ea
sabsiru <zero013@naver.com>
2025-06-15 16:24:21 +0900

    feat: Post Repository 추가
    각 클래스의 JpaRepository 추가함
    PostLike의 경우 중복 방지를 위해 비관적락 적용

--- src/main/java/consome/domain/post/PostLikeRepository.java
+++ src/main/java/consome/domain/post/PostLikeRepository.java
@@ -1,6 +1,17 @@
 package consome.domain.post;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PostLikeRepository  extends JpaRepository<PostLike, Long> {
+    List<PostLike> findByPostIdAndType(Long postId, LikeType type);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT pl FROM PostLike pl WHERE pl.postId = :postId AND pl.userId = :userId AND pl.type = :type AND pl.deleted = false")
+    Optional<PostLike> findByIdForUpdate(Long postId, Long userId, LikeType type);
 }
---
+++ src/main/java/consome/domain/post/PostViewRepository.java
@@ -0,0 +1,6 @@
+package consome.domain.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostViewRepository extends JpaRepository<PostView, Long> {
+}
