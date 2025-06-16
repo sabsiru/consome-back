8c998bab45290d33098d8bc640162871c87df62c
sabsiru <zero013@naver.com>
2025-06-16 17:00:42 +0900

    feat: 로그인아이디, 닉네임 중복확인 검증 추가
    UserService에서 로그인아이디와 닉네임 중복 메서드 추가
    추후에 중복 확인 api로 사용 가능할거같음.
    
    UserRepository에 검증 쿼리 추가

--- src/main/java/consome/domain/user/UserRepository.java
+++ src/main/java/consome/domain/user/UserRepository.java
@@ -2,6 +2,11 @@ package consome.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User> findByLoginId(String loginId);
+
+    List<User> findByNickname(String nickname);
 }
