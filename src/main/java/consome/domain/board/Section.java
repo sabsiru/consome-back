f347240f74fa6cbced7eb0667b7e23cc0d1e3af4
sabsiru <zero013@naver.com>
2025-07-02 15:56:01 +0900

    feat: JpaAuditing 사용을 위한 도메인 수정

--- src/main/java/consome/domain/board/Section.java
+++ src/main/java/consome/domain/board/Section.java
@@ -7,13 +7,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@EnableJpaAuditing
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Section {
 
