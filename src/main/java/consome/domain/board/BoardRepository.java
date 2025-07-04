339b0f47ab7283b0f80a3fed4911796f7cb1a25a
sabsiru <zero013@naver.com>
2025-07-04 14:07:11 +0900

    feat: Section, Board 조회 쿼리 추가

--- src/main/java/consome/domain/board/BoardRepository.java
+++ src/main/java/consome/domain/board/BoardRepository.java
@@ -2,6 +2,10 @@ package consome.domain.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
     boolean existsByName(String name);
+
+    List<Board> findBySectionIdAndDeletedFalseOrderByDisplayOrder(Long sectionId);
 }
