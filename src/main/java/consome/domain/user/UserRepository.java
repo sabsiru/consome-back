1f6d0f2c9d56154dd46502e86442518cc832ad78
sabsiru <zero013@naver.com>
2025-07-14 16:51:17 +0900

    feat: UserRepository 수정

--- src/main/java/consome/domain/user/UserRepository.java
+++ src/main/java/consome/domain/user/UserRepository.java
@@ -3,10 +3,15 @@ package consome.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findByLoginId(String loginId);
+    List<User> findAllByLoginId(String loginId);
 
-    List<User> findByNickname(String nickname);
+    List<User> findAllByNickname(String nickname);
+
+    User findByLoginId(String loginId);
+
+    Optional<User> findByLoginIdAndPassword(String loginId, String password);
 }
