0819986d06ca5f3b2de9c7f352c3230f44dc5fdd
sabsiru <zero013@naver.com>
2025-07-15 01:17:54 +0900

    feat: AdminController에서 사용할 dto 구현
    section, board, category에서 공통 적으로 사용되는
    rename 메서드에서 사용할 RenameRequest와
    section에서 응답할 SectionResponse 작성

---
+++ src/main/java/consome/interfaces/admin/dto/SectionResponse.java
@@ -0,0 +1,23 @@
+package consome.interfaces.admin.dto;
+
+
+import consome.domain.board.Section;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SectionResponse {
+
+    private Long id;
+    private String name;
+    private int displayOrder;
+
+    public static SectionResponse from(Section section) {
+        return new SectionResponse(
+                section.getId(),
+                section.getName(),
+                section.getDisplayOrder()
+        );
+    }
+}
