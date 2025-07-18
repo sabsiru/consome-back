c964bb9468261276ade563dbd830f6370bc67b94
sabsiru <zero013@naver.com>
2025-07-18 13:57:06 +0900

    feat: AdminController 에서 사용할 dto
    각 request 구현

--- src/main/java/consome/interfaces/admin/dto/CreateCategoryReqeust.java
+++ src/main/java/consome/interfaces/admin/dto/CreateCategoryReqeust.java
@@ -8,11 +8,13 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class CreateSectionRequest {
+public class CreateCategoryReqeust {
+
+    @NotBlank(message = "게시판을 선택해 주세요")
+    private Long boardId;
 
     @NotBlank(message = "이름을 입력해주세요.")
     @Size(min = 1, max = 20, message = "20자 이하로 입력해주세요.")
-
     private String name;
 
     @NotBlank(message = "순서를 입력해주세요.")
