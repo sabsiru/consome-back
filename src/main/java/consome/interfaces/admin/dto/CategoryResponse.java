61a52140f49aefadf9a829a8c3b6f703410327f2
sabsiru <zero013@naver.com>
2025-07-18 13:57:49 +0900

    feat: AdminController response 구현
    board, category

---
+++ src/main/java/consome/interfaces/admin/dto/CategoryResponse.java
@@ -0,0 +1,23 @@
+package consome.interfaces.admin.dto;
+
+
+import consome.domain.board.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CategoryResponse {
+
+    private Long id;
+    private String name;
+    private int displayOrder;
+
+    public static CategoryResponse from(Category category) {
+        return new CategoryResponse(
+                category.getId(),
+                category.getName(),
+                category.getDisplayOrder()
+        );
+    }
+}
