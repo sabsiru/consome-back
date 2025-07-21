5d79f76f6158674fd7b7c4c4fcdd8b44115b47ac
sabsiru <zero013@naver.com>
2025-07-21 16:54:56 +0900

    feat: 예외 핸들러 구현

---
+++ src/main/java/consome/interfaces/advice/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package consome.interfaces.advice;
+
+import consome.domain.user.exception.UserException;
+import consome.interfaces.error.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserException.DuplicateLoginId.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateLoginId(UserException.DuplicateLoginId ex) {
+        ErrorResponse body = new ErrorResponse(
+                ex.getCode(),
+                ex.getMessage()
+        );
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(body);
+    }
+}
---
+++ src/main/java/consome/interfaces/error/ErrorResponse.java
@@ -0,0 +1,11 @@
+package consome.interfaces.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final String code;
+    private final String message;
+}
