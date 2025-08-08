3f8d6ee7ada9978e52e24d395c2120cd179f2258
sabsiru <zero013@naver.com>
2025-08-08 15:34:44 +0900

    refactor: PostSummary Record 변경
    클래스 -> 레코드로 변경

--- src/main/java/consome/domain/post/PostSummary.java
+++ src/main/java/consome/domain/post/PostSummary.java
@@ -1,19 +1,15 @@
 package consome.domain.post;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-public class PostSummary {
-    private final Long postId;
-    private final String title;
-    private final Long authorId;
-    private final LocalDateTime createdAt;
-    private final int viewCount;
-    private final int likeCount;
-    private final int dislikeCount;
-    private final int commentCount;
+public record PostSummary(
+        Long postId,
+        String title,
+        Long authorId,
+        LocalDateTime createdAt,
+        int viewCount,
+        int likeCount,
+        int dislikeCount,
+        int commentCount
+) {
 }
