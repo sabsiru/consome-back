5f51e3b49a907d5196d6890c4ba4be982111dd87
sabsiru <zero013@naver.com>
2025-08-14 12:22:36 +0900

    feat: BoardFacade 메서드추가

--- src/main/java/consome/application/admin/BoardFacade.java
+++ src/main/java/consome/application/admin/BoardFacade.java
@@ -2,14 +2,23 @@ package consome.application.admin;
 
 import consome.domain.board.Board;
 import consome.domain.board.BoardService;
+import consome.domain.board.Category;
+import consome.domain.board.CategoryService;
+import consome.domain.post.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardFacade {
 
     private final BoardService boardService;
+    private final CategoryService categoryService;
+    private final PostService postService;
 
     public Board create(Long sectionId, String name, String description, int displayOrder) {
         return boardService.create(sectionId, name, description, displayOrder);
@@ -25,5 +34,23 @@ public class BoardFacade {
 
     public void delete(Long boardId) {
         boardService.delete(boardId);
+    }
+
+    public List<Category> getCategories(Long boardId) {
+        return categoryService.getCategories(boardId);
+    }
+
+    public Page<BoardPostsResult> getPosts(Long refBoardId, Pageable pageable) {
+        return postService.getPostByBoard(refBoardId, pageable)
+                .map(post -> new BoardPostsResult(
+                        post.postId(),
+                        post.title(),
+                        post.authorId(),
+                        post.createdAt(),
+                        post.viewCount(),
+                        post.likeCount(),
+                        post.dislikeCount(),
+                        post.commentCount()
+                ));
     }
 }
