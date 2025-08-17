e87ab4962b1032b3e1458394ea2779962d9dc03b
sabsiru <zero013@naver.com>
2025-08-17 20:59:01 +0900

    feat: NavigationFacade
    dto 적용

--- src/main/java/consome/interfaces/navigation/v1/NavigationController.java
+++ src/main/java/consome/interfaces/navigation/v1/NavigationController.java
@@ -1,8 +1,8 @@
 package consome.interfaces.navigation.v1;
 
 import consome.application.navigation.NavigationFacade;
-import consome.domain.board.Board;
-import consome.domain.board.Section;
+import consome.interfaces.navigation.dto.SectionHeaderResponse;
+import consome.interfaces.navigation.dto.BoardItemResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,17 +13,17 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1")
+@RequestMapping("/api/v1/navigation")
 public class NavigationController {
-    private final NavigationFacade mainFacade;
+    private final NavigationFacade navigationFacade;
 
     @GetMapping("/sections")
-    public List<Section> getSections() {
-        return mainFacade.getSections();
+    public List<SectionHeaderResponse> getSections() {
+        return SectionHeaderResponse.fromSections(navigationFacade.getSections());
     }
 
     @GetMapping("/sections/{sectionId}/boards")
-    public List<Board> getBoards(@PathVariable Long sectionId) {
-        return mainFacade.getBoards(sectionId);
+    public List<BoardItemResponse> getBoards(@PathVariable Long sectionId) {
+        return BoardItemResponse.fromBoards(navigationFacade.getBoards(sectionId));
     }
 }
