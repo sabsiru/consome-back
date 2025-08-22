fc9ceb0a02bdabc79e8e8cd84bc4b9bdc0805247
sabsiru <zero013@naver.com>
2025-08-22 12:52:17 +0900

    feat: UserFacade -> PostFacade 분리

--- src/main/java/consome/application/post/PostFacade.java
+++ src/main/java/consome/application/post/PostFacade.java
@@ -1,10 +1,10 @@
 package consome.application.post;
 
-
 import consome.domain.point.PointHistoryType;
 import consome.domain.point.PointService;
 import consome.domain.post.entity.Post;
 import consome.domain.post.PostService;
+import consome.domain.post.entity.PostStat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +22,40 @@ public class PostFacade {
         Post post = postService.post(command.refBoardId(), command.refCategoryId(), command.refUserId(), command.title(), command.content());
         return PostResult.of(post.getId());
 
+    }
+
+    @Transactional
+    public Post edit(String title, String content, Long postId, Long userId) {
+        return postService.edit(title, content, postId, userId);
+    }
+
+    @Transactional
+    public Post delete(Long postId, Long userId) {
+        pointService.penalize(userId, PointHistoryType.POST_DEL);
+        return postService.delete(postId, userId);
+    }
+
+    @Transactional
+    public PostStat like(Post post, Long userId) {
+        pointService.earn(post.getRefUserId(), PointHistoryType.POST_LIKE);
+        postService.like(post, userId);
+
+        return postService.getPostStat(post.getId());
+    }
+
+    @Transactional
+    public PostStat dislike(Post post, Long userId) {
+        postService.dislike(post, userId);
+        pointService.penalize(post.getRefUserId(), PointHistoryType.POST_DISLIKE);
+
+        return postService.getPostStat(post.getId());
+    }
+
+    @Transactional
+    public PostStat increaseViewCount(Long postId, String userIp, Long userId) {
+        postService.increaseViewCount(postId, userIp, userId);
+
+        return postService.getPostStat(postId);
     }
 
 }
