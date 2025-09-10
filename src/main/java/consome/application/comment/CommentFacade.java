2b596a5575ebef34d1c989c1e311cd1929a6e474
sabsiru <zero013@naver.com>
2025-09-10 12:31:09 +0900

    feat: commentFacade 추가

---
+++ src/main/java/consome/application/comment/CommentFacade.java
@@ -0,0 +1,72 @@
+package consome.application.comment;
+
+import consome.domain.comment.Comment;
+import consome.domain.comment.CommentReaction;
+import consome.domain.comment.CommentService;
+import consome.domain.point.PointHistoryType;
+import consome.domain.point.PointService;
+import consome.domain.post.PostService;
+import consome.domain.post.ReactionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentFacade {
+
+    private final CommentService commentService;
+    private final PointService pointService;
+    private final PostService postService;
+
+    @Transactional
+    public Comment comment(Long postId, Long userId, Long parentId, String content) {
+        postService.getPost(postId);
+        postService.increaseCommentCount(postId);
+        Comment comment = commentService.comment(postId, userId, parentId, content);
+        pointService.earn(userId, PointHistoryType.COMMENT_WRITE);
+        return comment;
+    }
+
+    @Transactional
+    public Comment editComment(Long userId, Long commentId, String content) {
+        Comment edit = commentService.edit(userId, commentId, content);
+
+        return edit;
+    }
+
+    @Transactional
+    public Comment deleteComment(Long userId, Long commentId) {
+        pointService.penalize(commentId, PointHistoryType.COMMENT_DEL);
+        return commentService.delete(userId, commentId);
+    }
+
+    @Transactional
+    public long likeComment(Long commentId, Long userId) {
+        commentService.like(commentId, userId);
+        pointService.earn(userId, PointHistoryType.COMMENT_LIKE);
+
+        return commentService.countReactions(commentId, ReactionType.LIKE);
+    }
+
+    @Transactional
+    public long dislikeComment(Long commentId, Long userId) {
+        commentService.dislike(commentId, userId);
+        pointService.penalize(userId, PointHistoryType.COMMENT_DISLIKE);
+
+        return commentService.countReactions(commentId, ReactionType.DISLIKE);
+    }
+
+    @Transactional
+    public long cancelReactionComment(Long commentId, Long userId) {
+        CommentReaction commentReaction = commentService.cancel(commentId, userId);
+        if (commentReaction.getType() == ReactionType.LIKE) {
+            pointService.penalize(userId, PointHistoryType.COMMENT_LIKE_CANCEL);
+            return commentService.countReactions(commentId, ReactionType.LIKE);
+        } else if (commentReaction.getType() == ReactionType.DISLIKE) {
+            pointService.penalize(userId, PointHistoryType.COMMENT_DISLIKE_CANCEL);
+            return commentService.countReactions(commentId, ReactionType.DISLIKE);
+        }
+        return 0;
+    }
+}
