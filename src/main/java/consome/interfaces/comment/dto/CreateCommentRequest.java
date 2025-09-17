22b041490a14cdc533185218061cf323eb789676
sabsiru <zero013@naver.com>
2025-09-17 17:36:30 +0900

    fix: @notblank 제거

--- src/main/java/consome/interfaces/comment/dto/CreateCommentRequest.java
+++ src/main/java/consome/interfaces/comment/dto/CreateCommentRequest.java
@@ -1,10 +1,8 @@
 package consome.interfaces.comment.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
 public record CreateCommentRequest(
-        @NotBlank Long userId,
-        @NotBlank Long parentId,
-        @NotBlank String content
+        Long userId,
+        Long parentId,
+        String content
 ) {
 }
