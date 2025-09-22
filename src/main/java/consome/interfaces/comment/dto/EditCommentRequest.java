7eedd7223481105606f4bbe7e06a5ec8606d5921
sabsiru <zero013@naver.com>
2025-09-22 17:58:20 +0900

    fix: @NotBlank 제거
    @NotBlank는 String 전용이다..

--- src/main/java/consome/interfaces/comment/dto/EditCommentRequest.java
+++ src/main/java/consome/interfaces/comment/dto/EditCommentRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record EditCommentRequest(
-        @NotBlank Long userId,
+        Long userId,
         @NotBlank @Size(max = 500) String content
 ) {
 }
