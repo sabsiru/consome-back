f8b7f4b60c0774585a42621a3dbda6a49a893526
sabsiru <zero013@naver.com>
2025-10-24 18:00:44 +0900

    order(0)

--- src/main/java/consome/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/consome/infrastructure/config/security/SecurityConfig.java
@@ -2,12 +2,14 @@ package consome.infrastructure.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Order(0)
 public class SecurityConfig {
 
     @Bean
