0e20c35afcf9235f960ee261a00a628c31513608
sabsiru <zero013@naver.com>
2025-12-10 17:58:05 +0900

    feat: 회원 관리

--- src/main/java/consome/interfaces/admin/v1/AdminV1ManageController.java
+++ src/main/java/consome/interfaces/admin/v1/AdminV1ManageController.java
@@ -1,9 +1,13 @@
 package consome.interfaces.admin.v1;
 
 import consome.application.admin.ManageFacade;
+import consome.application.admin.UserPagingResult;
 import consome.application.admin.result.ManageTreeResult;
 import consome.interfaces.admin.dto.manage.ManageTreeResponse;
+import consome.interfaces.admin.dto.manage.ManageUserListResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +24,13 @@ public class AdminV1ManageController {
     public ResponseEntity<ManageTreeResponse> getTree() {
         ManageTreeResult result = manageFacade.getTree();
         return ResponseEntity.ok(ManageTreeResponse.from(result));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<ManageUserListResponse> getUsers(@PageableDefault(size= 20) Pageable pageable) {
+        System.out.println("[AdminV1ManageController] /api/v1/admin/manage/users page="
+                + pageable.getPageNumber() + ", size=" + pageable.getPageSize());
+        UserPagingResult result = manageFacade.getUsers(pageable);
+        return ResponseEntity.ok(ManageUserListResponse.from(result));
     }
 }
