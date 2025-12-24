465f0916a7b0ac13527552a6d738b4c7f097f227
sabsiru <zero013@naver.com>
2025-12-25 04:53:21 +0900

    fix: 참조값 수정

--- src/main/java/consome/interfaces/post/dto/PostRequest.java
+++ src/main/java/consome/interfaces/post/dto/PostRequest.java
@@ -1,9 +1,9 @@
 package consome.interfaces.post.dto;
 
 public record PostRequest(
-        Long refUserId,
         Long boardId,
         Long categoryId,
+        Long userId,
         String title,
         String content
 ) {
