4256a81b83f919980ca6478e31c21700b486740e
sabsiru <zero013@naver.com>
2026-01-01 17:00:03 +0900

    fix: 참조값 변경

--- src/main/java/consome/application/post/PostFacade.java
+++ src/main/java/consome/application/post/PostFacade.java
@@ -6,11 +6,13 @@ import consome.domain.post.entity.Post;
 import consome.domain.post.PostService;
 import consome.domain.post.entity.PostStat;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostFacade {
 
     private final PostService postService;
@@ -18,8 +20,12 @@ public class PostFacade {
 
     @Transactional
     public PostResult post(PostCommand command) {
-        pointService.earn(command.refUserId(), PointHistoryType.POST_WRITE);
-        Post post = postService.post(command.refBoardId(), command.refCategoryId(), command.refUserId(), command.title(), command.content());
+        pointService.earn(command.userId(), PointHistoryType.POST_WRITE);
+
+        log.info("BoardId: {}, CategoryId: {}, UserId: {}, Title: {}, Content: {}",
+                command.boardId(), command.categoryId(), command.userId(), command.title(), command.content());
+
+        Post post = postService.post(command.boardId(), command.categoryId(), command.userId(), command.title(), command.content());
         return PostResult.of(post.getId());
 
     }
